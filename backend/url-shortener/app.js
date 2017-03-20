1c70ac6e8aa7c102a023df6851f361d3780ef621
Пользователь <>
2017-03-21 00:55:59 +0500

    Add URL Shortener

---
+++ backend/url-shortener/app.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+      fs  = require('fs');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  var usage = 'Usage:' +
+              '\n /new/[url] creates a short link to [url]' +
+              '\n /[shortlink] redirects to [url]';
+  res.write(usage);
+  res.end();
+})
+
+app.get('/new/:url*', (req, res) => {
+    var url = req.params.url + req.params[0];
+
+    var db = JSON.parse(fs.readFileSync('local-db.json'));
+    db[++db.count] = url;
+
+    fs.writeFileSync('local-db.json', JSON.stringify(db));
+
+    res.json({
+      "original_url": url,
+      "short_url": db.count
+    });
+});
+
+app.get('/:shorturl', (req, res) => {
+  var shortlink = req.params.shorturl;
+
+  var db = JSON.parse(fs.readFileSync('local-db.json'));
+  if(db[shortlink] && db[shortlink] !== 'count') {
+    res.redirect(db[shortlink]);
+  }
+  else {
+    var msg = 'Invalid URL. Try one of these: ' + JSON.stringify(db,undefined,2);
+    res.write(msg);
+    res.end();
+  }
+})
+
+app.listen(3000);
